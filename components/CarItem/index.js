1292532cebdbee215a9ffd3e86cd59c4024a6610
kingdennis-crypto <dennismoes@me.com>
2021-05-08 13:15:33 +0200

    Added a type to the button calling

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
+import StyledButton from "../StyledButton"
 import styles from './styles';
 
 const CarItem = (props) => {
@@ -10,6 +11,8 @@ const CarItem = (props) => {
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subtitle}>Starting at $69,420</Text>
         </View>
+
+        <StyledButton type="primary"/>
       </View>
     );
 };
